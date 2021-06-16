c036ddf93ef5dde76da03f9d8440ccb4353b8c7d
gdescomps <46891683+gdescomps@users.noreply.github.com>
2021-06-16 14:30:09 +0200

    Initializing the shell

--- main.c
+++ main.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "init_shell.c"
+
 int main(int argc, char *argv[]) {
-	char str[255];
-	scanf("%[^\n]%*c", str);
-	printf("%s\n", str);
+
+	init_shell();
+
+	char user_input[255];
+
+	while(strcmp(user_input,"quit")){
+		
+		scanf("%[^\n]%*c", user_input);
+		printf("%s\n", user_input);
+	}
 }
